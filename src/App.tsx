d88c9b19ef0c949927bedef41c4d9d89293c9371
Armen Kesablyan <armen@twoowl.com>
2020-10-24 14:53:44 -0700

    updates latest

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,193 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './logo.png';
 import './App.css';
+import { Button, TextField } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import repTemplates from './templates';
+import reps from './myreps.json';
+import CopyToClipboard from 'react-copy-to-clipboard';
+
+type Rep = typeof reps.reps;
 
 function App() {
+
+  const [zip, setZip] = useState('');
+  const [zipError, setZipError] = useState(false);
+  const [city, setCity] = useState('');
+  const [cityError, setCityError] = useState(false);
+  const [name, setName] = useState('');
+  const [nameError, setNameError] = useState(false);
+  const [cStep, setCStep] = useState(0);
+
+  const goTo = (url: string) => {
+    window.open(url, '_blank');
+  }
+
+  const validateForm = (): boolean => {
+    let isValid = true;
+    if (name === '') {
+      setNameError(true);
+      isValid = false;
+    }
+    if (city === '') {
+      setCityError(true);
+      isValid = false;
+    }
+    if (zip === '') {
+      setZipError(true);
+      isValid = false;
+    }
+    if (zip.length !== 5) {
+      setZipError(true);
+      isValid = false;
+    }
+    return isValid;
+  }
+
+
+  const findReps = () => {
+    if (validateForm()) {
+      setCStep(1);
+    }
+  }
+
+  const formStep = () => {
+    return (<div className="form-step">
+    <p>Complete fields below to auto-populate individualized scripts to send to your governor, senators, and congressman.</p>
+    <div className="zipInput">
+    <Grid container spacing={3}>
+    <Grid item xs={12}>
+        <TextField
+        id="name"
+        onChange={(t) => {setNameError(false); setName(t.target.value)} }
+        error={nameError}
+        helperText={nameError ? 'Please enter your name' : ''}
+        placeholder="Your full name" 
+        label="Full Name" 
+        variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField 
+        id="city" 
+        onChange={(t) => {setCityError(false); setCity(t.target.value)} }
+        error={cityError}
+        helperText={cityError ? 'Please enter your city' : ''}
+        placeholder="Your city" 
+        label="City" 
+        variant="outlined" />
+      </Grid>
+      <Grid item xs={12}>
+      <TextField 
+        id="zip" 
+        onChange={(t) => { setZipError(false); setZip(t.target.value)}}
+        error={zipError}
+        helperText={zipError ? 'Please enter a valid zip code' : ''}
+        type="number"
+        placeholder="Your zip code" 
+        label="Zip Code" 
+        variant="outlined" />
+        {/* </ReactInputMask> */}
+      </Grid>
+    </Grid>
+    </div>
+    <Button variant="contained" color="primary" onClick={() => findReps()}>
+      Contact My Reps
+    </Button>
+  </div>)
+  }
+
+
+  const formReps = (zipCode: string) => {
+    return (<div className="reps">
+      <Grid container justify="center" style={{maxWidth: 960}}>
+      <div className="body-content" style={{ textAlign: 'left'}}>
+        <h2>Instructions</h2>
+      <strong>Step 1</strong>: Click "COPY SCRIPT" to copy the script for the specific representative.
+      <br /><br />
+      <strong>Step 2</strong>: Click "CONTACT WEBSITE" to go to representative’s contact website.
+    <br /><br />
+    <strong>Step 3</strong>: You will be taken to the representative’s contact website where you will paste the copied script.  If the representative’s website asks you to choose a topic, select anything similar to "Legislation Issues", "Foreign Relations", or "Foreign Affairs".
+    <br /><br />
+    <div className="insta-link">
+        <p><i>
+        For help with this website or process, please direct message <a href="https://www.instagram.com/williambgdoian/">@WilliamBgdoian</a> on Instagram. We need to work together to make this work.
+        </i>
+        </p>
+      </div>
+      </div>
+      </Grid>
+      <hr />
+    <Grid container justify="center" style={{maxWidth: 960}}>
+      {reps.reps.filter((itm) => {
+      return itm.zips.find((val) => val === +zipCode) ? true : false;
+    }).map((itm) => {
+      // @ts-ignore
+      const tmpl = repTemplates[(itm.template_id ? itm.template_id : itm.category)] || (() => '');
+      const rndrd = tmpl({rep: itm.name, city, name});
+    return (<Grid item xs={12}>
+      <div className="rep-paper" title="Governer">
+        <div className="rep-info">
+          <div className="category">
+            <img src={`/headshots/${itm.name.toLocaleLowerCase().replace(' ', '-')}.jpg`} width="140" alt="Gavin Newsom" />
+          </div>
+          <p>
+            <b className="upper">{itm.category}</b>
+            <br />
+            {itm.name}
+          </p>
+          <p>
+            <CopyToClipboard text={rndrd.replace(/<br \/><br \/>/g, '')}>
+             <Button 
+            variant="contained">
+              Copy Script
+              </Button>
+          </CopyToClipboard>
+          </p>
+          <p>
+            <Button 
+            onClick={() => goTo(itm.link)}
+            variant="contained">
+              Contact Website
+              </Button>
+          </p>
+        </div>
+        <div className="rep-template">
+          <div className="template-info" dangerouslySetInnerHTML={{__html: rndrd}}>
+            
+          </div>
+        </div>
+      </div>
+    </Grid>)})}
+    </Grid>
+    <div className="insta-link insta-footer">
+        For help with this website or process, please direct message <a href="https://www.instagram.com/williambgdoian/">@WilliamBgdoian</a> on Instagram. We need to work together to make this work.
+        <br /><br />
+        <a href="https://www.instagram.com/williambgdoian/">
+        <img width="20" src='/instagram-icon.png' alt="instagram" /> @WilliamBgdoian 
+        </a>
+        </div>
+    </div>)
+  }
+
+  const currentStep = (step: number) => {
+    switch(step) {
+      case 0: 
+        return formStep();
+      case 1:
+        return formReps(zip);
+      default:
+
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {currentStep(cStep)}
       </header>
+      
     </div>
   );
 }
---
+++ src/templates.ts
@@ -0,0 +1,109 @@
+
+
+export const congressman = (p: Record<string, string>) => `
+
+Hello Congressman ${p.rep}, 
+<br /><br />
+My name is ${p.name} and I am calling on you from ${p.city}, CA.
+<br /><br />
+Azerbaijan and Turkey are attacking democratic Armenia and Artsakh (Nagorno Karabakh), and full-fledged war is being waged against the Armenian people, a people that survived genocide during WW1. Armenian civilians are being murdered and removed from their lands, and their existence is once again in grave danger.  Turkey and Azerbaijan are bombing civilians and have brought Jihadist terrorists to fight against the Armenian people. We are at dire risk of experiencing and witnessing a second genocide against the Armenian people.  Turkey’s president has also alluded to invading Jerusalem.  To avoid this conflict becoming a drawn-out regional and international catastrophe and genocide, you need to act right now.
+<br /><br />
+I am calling on you to:
+<br /><br />
+1.	Publicly, through ALL social media platforms, condemn Azerbaijani and Turkish attacks on Artsakh and Armenia;
+<br /><br />
+2.	Publicly, through ALL social media platforms, recognize the independence of Artsakh;
+<br /><br />
+3.	Explicitly call for immediately halting ALL U.S. military and financial aid to the anti-American, anti-Armenian, and anti-Israeli dictatorships of Azerbaijan and Turkey;
+<br /><br />
+4.	Introduce a legislative bill to end the Section 907 waiver of the Freedom Support Act in order to completely terminate any U.S. military and financial aid and exports to Azerbaijan;
+<br /><br />
+5.	Call for the immediate enforcement of the Magnitsky Act sanctions against Erdogan, Aliyev, and all other government officials of Turkey and Azerbaijan; and
+<br /><br />
+6.	Introduce a legislative bill with immediate crippling sanctions against Turkey and Azerbaijan in order to deploy tangible and decisive pressures that will force a halt in attacks and crimes against humanity committed on the people of Artsakh and Armenia.
+<br /><br />
+Turkey and Azerbaijan have been committing human rights violations dropping cluster bombs on civilians for days now and I have not seen any effective legislative action from you.  I suggest you act with the utmost urgency to rectify this egregious oversight.  
+<br /><br />
+It is critical for you to note that I am registered to vote and ready to make myself heard through my vote. Your actions regarding this immediate issue will absolutely play a deciding role during your upcoming election.
+<br /><br />
+Thank you.`
+
+export const dianne = (p: Record<string, string>) => `
+Hello Senator ${p.rep}, 
+<br /><br /> 
+My name is ${p.name} and I am calling on you from ${p.city}, CA.
+<br /><br />    
+Azerbaijan and Turkey are attacking democratic Armenia and Artsakh (Nagorno Karabakh), and full-fledged war is being waged against the Armenian people, a people that survived genocide during WW1. Armenian civilians are being murdered and removed from their lands, and their existence is once again in grave danger.  Turkey and Azerbaijan are bombing civilians and have brought Jihadist terrorists to fight against the Armenian people. We are at dire risk of experiencing and witnessing a second genocide against the Armenian people.  Turkey’s president has also alluded to invading Jerusalem.  To avoid this conflict becoming a drawn-out regional and international catastrophe and genocide, you need to act right now.
+<br /><br />    
+I am calling on you to:
+<br /><br />
+1.	Publicly, through ALL social media platforms, condemn Azerbaijani and Turkish attacks on Artsakh and Armenia;
+<br /><br />
+2.	Publicly, through ALL social media platforms, recognize the independence of Artsakh;
+<br /><br />
+3.	Explicitly call for immediately halting ALL U.S. military and financial aid to the anti-American, anti-Armenian, and anti-Israeli dictatorships of Azerbaijan and Turkey;
+<br /><br />
+4.	Introduce a legislative bill to end the Section 907 waiver of the Freedom Support Act in order to completely terminate any U.S. military and financial aid and exports to Azerbaijan;
+<br /><br />
+5.	Call for the immediate enforcement of the Magnitsky Act sanctions against Erdogan, Aliyev, and all other government officials of Turkey and Azerbaijan; and
+<br /><br />
+6.	Introduce a legislative bill with immediate crippling sanctions against Turkey and Azerbaijan in order to deploy tangible and decisive pressures that will force a halt in attacks and crimes against humanity committed on the people of Artsakh and Armenia.
+<br /><br />    
+Turkey and Azerbaijan have been committing human rights violations dropping cluster bombs on civilians for days now and I have not seen any effective legislative action from you.  I suggest you act with the utmost urgency to rectify this egregious oversight.  
+<br /><br />    
+It is critical for you to note that I am registered to vote and ready to make myself heard through my vote. Your actions regarding this immediate issue will absolutely play a deciding role during your re-election efforts in 2024.
+<br /><br />    
+Thank you.`;
+
+export const governor = (p: Record<string, string>) => `
+Hello Governor ${p.rep}, 
+<br /><br />    
+My name is ${p.name} and I am calling on you from ${p.city}, CA.
+<br /><br />       
+Azerbaijan and Turkey are attacking democratic Armenia and Artsakh (Nagorno Karabakh), and full-fledged war is being waged against the Armenian people, a people that survived genocide during WW1. Armenian civilians are being murdered and removed from their lands, and their existence is once again in grave danger.  Turkey and Azerbaijan are bombing civilians and have brought Jihadist terrorists to fight against the Armenian people. We are at dire risk of experiencing and witnessing a second genocide against the Armenian people.  Turkey’s president has also alluded to invading Jerusalem.  To avoid this conflict becoming a drawn-out regional catastrophe you need to act right now.
+<br /><br />       
+I am calling on you to demand that you publicly, through ALL social media platforms, condemn Azerbaijani and Turkish attacks and explicitly call for immediately stopping ALL U.S. military aid to the anti-American, anti-Armenian, and anti-Israeli dictatorships in Azerbaijan and Turkey.
+<br /><br />       
+I am calling on you, and the state of California, to publicly recognize Artsakh's independence through ALL social media platforms and through official proclamation.
+<br /><br />       
+Finally, I am expecting to see you immediately call for crippling sanctions against Turkey and Azerbaijan in order to deploy tangible pressures onto Turkey and Azerbaijan to force a halt in attacks against Artsakh and Armenia.  
+<br /><br />       
+Turkey and Azerbaijan have been committing human rights violations dropping cluster bombs on civilians for days now and I have not seen any legislative action from you.  I suggest you act with the utmost urgency to rectify this egregious oversight.   
+<br /><br />       
+It is critical for you to note that I am registered to vote and ready to make myself heard through my vote. Your actions regarding this immediate issue will absolutely play a deciding role during your 2022 election.
+<br /><br />       
+Thank you.`
+
+export const kamala = (p: Record<string, string>) => `
+Hello Senator ${p.rep}, 
+<br /><br />
+My name is ${p.name} and I am calling on you from ${p.city}, CA.
+<br /><br />
+Azerbaijan and Turkey are attacking democratic Armenia and Artsakh (Nagorno Karabakh), and full-fledged war is being waged against the Armenian people, a people that survived genocide during WW1. Armenian civilians are being murdered and removed from their lands, and their existence is once again in grave danger.  Turkey and Azerbaijan are bombing civilians and have brought Jihadist terrorists to fight against the Armenian people. We are at dire risk of experiencing and witnessing a second genocide against the Armenian people.  Turkey’s president has also alluded to invading Jerusalem.  To avoid this conflict becoming a drawn-out regional and international catastrophe and genocide, you need to act right now.
+<br /><br />
+I am calling on you to:
+<br /><br />
+1.	Publicly, through ALL social media platforms, condemn Azerbaijani and Turkish attacks on Artsakh and Armenia;
+<br /><br />
+2.	Publicly, through ALL social media platforms, recognize the independence of Artsakh;
+<br /><br />
+3.	Explicitly call for immediately halting ALL U.S. military and financial aid to the anti-American, anti-Armenian, and anti-Israeli dictatorships of Azerbaijan and Turkey;
+<br /><br />
+4.	Introduce a legislative bill to end the Section 907 waiver of the Freedom Support Act in order to completely terminate any U.S. military and financial aid and exports to Azerbaijan;
+<br /><br />
+5.	Call for the immediate enforcement of the Magnitsky Act sanctions against Erdogan, Aliyev, and all other government officials of Turkey and Azerbaijan; and
+<br /><br />
+6.	Introduce a legislative bill with immediate crippling sanctions against Turkey and Azerbaijan in order to deploy tangible and decisive pressures that will force a halt in attacks and crimes against humanity committed on the people of Artsakh and Armenia.
+<br /><br />
+Turkey and Azerbaijan have been committing human rights violations dropping cluster bombs on civilians for days now and I have not seen any effective legislative action from you.  I suggest you act with the utmost urgency to rectify this egregious oversight.  
+<br /><br />
+It is critical for you to note that I am registered to vote and ready to make myself heard through my vote. Your actions regarding this immediate issue will absolutely play a deciding role during your upcoming election and your potential re-election efforts in 2022.
+<br /><br />
+Thank you.`
+
+export default {
+    "congressman": congressman,
+    "dianne": dianne,
+    "governor": governor,
+    "kamala": kamala,
+};
